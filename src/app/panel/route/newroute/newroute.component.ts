71a67b69f1dc0cff0fd19f693d11e5be1c21fa02
Guilherme Pinto <guilhermecamachop@gmail.com>
2018-03-06 21:48:33 -0300

    New routes form listing mysql database, table, columns

--- src/app/panel/route/newroute/newroute.component.ts
+++ src/app/panel/route/newroute/newroute.component.ts
@@ -12,11 +12,16 @@ declare var $: any;
 export class NewrouteComponent implements OnInit {
 
   drivers;
-  databases = false;
-  table = false;
+  databases = [];
+  table = [];
   database;
   item: FormGroup;
-  column = false;
+  column = [];
+  requests: any;
+  blocktable = true;
+  blockcolumns = true;
+  request = [];
+
 
   constructor(private api: ApiService) {
   }
@@ -28,23 +33,54 @@ export class NewrouteComponent implements OnInit {
       database: new FormControl(''),
       tables: new FormControl('')
     });
+    this.request['get'] = true;
+    this.request['post'] = true;
+    this.request['put'] = true;
+    this.request['delete'] = true;
+
     localStorage.removeItem('database');
     this.item.get('database').valueChanges.subscribe(data => console.log(data));
+
     const bomb = this;
     $('#database').on('change', function () {
-      bomb.searchtables($(this).val());
       localStorage.setItem('database', $(this).val());
+      console.log('Database: ' + $(this).val());
+      const database = $(this).val();
+      this.blocktable = false;
+      setTimeout(
+        function () {
+          bomb.searchtables(database);
+          bomb.blocktable = true;
+        }, 200);
     });
+
+    $('#tables').on('change', function () {
+      localStorage.setItem('tables', $(this).val());
+      console.log('Table: ' + $(this).val());
+      const table = $(this).val();
+      this.blockcolumns = false;
+      setTimeout(
+        function () {
+          bomb.searchcolumns(localStorage.getItem('database'), table);
+          bomb.blockcolumns = true;
+        }, 200);
+    });
+  }
+
+  active(request) {
+    console.log(request);
+    this.request[request] = !this.request[request];
   }
 
   startSelectBox(id) {
     setTimeout(
       function () {
         $(id).material_select();
-      }, 500);
+      }, 300);
   }
 
   getListDrivers() {
+    this.drivers = [];
     const result = (this.api.getListDrivers());
     result.then(res => {
       this.drivers = res.drivers;
@@ -52,25 +88,37 @@ export class NewrouteComponent implements OnInit {
   }
 
   searchtables(database) {
+    if (!database) {
+      database = localStorage.getItem('database');
+    }
+    console.log('DataBase: ' + database);
+    this.table = [];
     const result = (this.api.searchTables(database));
     result.then(res => {
       this.table = res;
-      // console.log(res);
       this.startSelectBox('#tables');
       const bomb = this;
+      this.blockcolumns = false;
       setTimeout(
         function () {
-          bomb.searchcolumns($('#database').find('option:first-child').val(), $('#tables').find('option:first-child').val());
-        }, 500);
+          localStorage.setItem('tables', $('#tables').find('option:first-child').val());
+          let Sdatabase = localStorage.getItem('database');
+          if (!Sdatabase) {
+            Sdatabase = $('#database').find('option:first-child').val();
+          }
+          bomb.searchcolumns(Sdatabase, $('#tables').find('option:first-child').val());
+          bomb.blockcolumns = true;
+        }, 300);
     });
   }
 
   searchcolumns(database, table) {
-    console.log('D:' + database + ' T:' + table);
+    this.column = [];
+    console.log('colunms \n Database:' + database + ' - Table: ' + table);
     if (database && table) {
       const result = (this.api.searchColumns(database, table));
       result.then(res => {
-        console.log(res);
+        // console.log(res);
         this.column = res;
         this.startSelectBox('#columns');
       });
@@ -78,16 +126,19 @@ export class NewrouteComponent implements OnInit {
   }
 
   searchdatabase() {
+    this.databases = [];
     const result = (this.api.searchDatabases());
     result.then(res => {
       this.databases = res;
       this.startSelectBox('#database');
       const bomb = this;
+      this.blocktable = false;
       setTimeout(
         function () {
           bomb.searchtables($('#database').find('option:first-child').val());
-        }, 500);
+          localStorage.setItem('database', $('#database').find('option:first-child').val());
+          bomb.blocktable = true;
+        }, 300);
     });
   }
-
 }
